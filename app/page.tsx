a17d64a803b8f9eba11441f41f3050d7f1f804ad
azharanggakusuma <azharanggakusuma01@gmail.com>
2025-12-21 19:45:47 +0700

    main: Updated

--- app/page.tsx
+++ app/page.tsx
@@ -123,15 +123,17 @@ export default function TranskripPage() {
         <table className="w-full text-[9px] border-collapse border border-black mb-2">
           <thead>
             <tr className="bg-[#D9EAF7] text-center font-bold h-5 border-b border-black">
-              <th className="border border-black w-6">No</th>
-              {/* PERBAIKAN: Lebar kolom Kode MK diperbesar dari w-16 menjadi w-24 */}
-              <th className="border border-black w-24">Kode MK</th>
+              {/* PERBAIKAN: Kolom No diperlebar (w-8) */}
+              <th className="border border-black w-8">No</th>
+              {/* PERBAIKAN: Kolom Kode MK diperlebar (w-32) */}
+              <th className="border border-black w-32">Kode MK</th>
               <th className="border border-black text-left pl-2">Mata Kuliah</th>
-              <th className="border border-black w-8">SMT</th>
-              <th className="border border-black w-8">SKS</th>
-              <th className="border border-black w-8">HM</th>
-              <th className="border border-black w-8">AM</th>
-              <th className="border border-black w-8">NM</th>
+              {/* PERBAIKAN: Kolom nilai diperlebar (w-10) */}
+              <th className="border border-black w-10">SMT</th>
+              <th className="border border-black w-10">SKS</th>
+              <th className="border border-black w-10">HM</th>
+              <th className="border border-black w-10">AM</th>
+              <th className="border border-black w-10">NM</th>
             </tr>
           </thead>
           <tbody>

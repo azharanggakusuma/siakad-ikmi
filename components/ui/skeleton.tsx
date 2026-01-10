82e29944b3a82496afc15147b345496fe5fbcf34
azharanggakusuma <azharanggakusuma01@gmail.com>
2026-01-10 15:20:54 +0700

    main: Updated

--- components/ui/skeleton.tsx
+++ components/ui/skeleton.tsx
@@ -1,14 +1,25 @@
 import { cn } from "@/lib/utils"
+import SkeletonLib, { SkeletonTheme } from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 
 function Skeleton({
   className,
   ...props
 }: React.HTMLAttributes<HTMLDivElement>) {
   return (
-    <div
-      className={cn("animate-pulse rounded-md bg-gray-200", className)}
-      {...props}
-    />
+    <SkeletonTheme baseColor="#e2e8f0" highlightColor="#f1f5f9">
+      <div
+        className={cn("overflow-hidden leading-none", className)}
+        {...props}
+      >
+        <SkeletonLib 
+          containerClassName="block h-full w-full" 
+          className="block h-full w-full" 
+          height="100%"
+          duration={1.5}
+        />
+      </div>
+    </SkeletonTheme>
   )
 }
 
